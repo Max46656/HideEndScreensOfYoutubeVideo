f37ad3eabc7f0919dc69809ded3f7f237e0932a1
Max46656 <117962384+Max46656@users.noreply.github.com>
2024-05-12 22:08:04 +0800

    1.0
    使用觀察者模式監視Youtube影片<video>底下的元素變化，
    並刪除符合結束畫面元素名稱的元素。
    分為新分頁與同分頁兩種方法。

---
+++ 隱藏youtube片尾畫面-2024-04-26.js
@@ -0,0 +1,77 @@
+// ==UserScript==
+// @name         隱藏youtube片尾畫面
+// @name:jp      YouTubeの終了画面を非表示にする。
+// @name:en      Hide end-screens of YouTube.
+// @namespace    http://tampermonkey.net/
+// @version      2024-04-26
+// @description  自動隱藏 YouTube 影片結尾
+// @description:en Automatically hide end-screens at the end of YouTube videos.
+// @description:jp YouTube動画の最後にある終了画面を自動的に非表示にします。
+// @author       Max
+// @match        https://www.youtube.com/
+// @match        https://www.youtube.com/watch?v=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+class youTubeRecommendationHider {
+    constructor(recommendedVideosClass,maxRecommend,maxCheckCount,videoSrc = null,recommendedVideosCount=0) {
+        this.recommendedVideosClass = recommendedVideosClass;
+        this.maxRecommend = maxRecommend;
+        this.maxCheckCount = maxCheckCount;
+        this.maxCheckCountReseter=maxCheckCount;
+        this.videoSrc = videoSrc;
+        this.recommendedVideosCount = recommendedVideosCount;
+        this.videoPlayer = document.querySelector('video');
+    }
+
+    hideRecommendVideos() {
+        let allRecommendedViedo = document.querySelectorAll(this.recommendedVideosClass);
+        allRecommendedViedo.forEach(element => {
+            console.log('刪除' + element.querySelector('a').href);
+            element.remove();
+            this.recommendedVideosCount++;
+            console.log('刪除推薦影片數' + this.recommendedVideosCount);
+        });
+        if (this.recommendedVideosCount < this.maxRecommend && this.maxCheckCount > 0) {
+            this.maxCheckCount--;
+            console.log('剩餘檢查次數' + this.maxCheckCount);
+            setTimeout(() => this.hideRecommendVideos(), 1000);
+        }
+    }
+
+    resetVariables() {
+        console.log('重置變數');
+        this.recommendedVideosCount = 0;
+        this.maxCheckCount = this.maxCheckCountReseter;
+        this.hideRecommendVideos();
+    }
+
+    newTab() {
+        if (this.videoSrc == null) {
+            console.log('新YouTube分頁');
+            this.hideRecommendVideos();
+        }
+    }
+
+    sameTab() {
+        if (this.videoPlayer) {
+            this.videoSrc = this.videoPlayer.src;
+            console.log('影片偵測<video>' + this.videoSrc);
+            const observer = new MutationObserver(mutations => {
+                mutations.forEach(mutation => {
+                    if (mutation.attributeName === 'src' && this.videoPlayer.src !== this.videoSrc) {
+                        console.log('新影片偵測' + this.videoPlayer.src);
+                        this.videoSrc = this.videoPlayer.src;
+                        this.resetVariables();
+                    }
+                });
+            });
+            observer.observe(this.videoPlayer, { attributes: true });
+        }
+    }
+}
+
+const johnTheCleaner=new youTubeRecommendationHider('.ytp-ce-element',4,3);
+johnTheCleaner.newTab();
+johnTheCleaner.sameTab();
